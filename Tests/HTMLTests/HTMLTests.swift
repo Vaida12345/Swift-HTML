acd7adda6b40d763d19a95ad9a4c42eed3f71b1d
Vaida <91354917+Vaida12345@users.noreply.github.com>
2022-11-11 21:46:43 +1100

    Initial Commit

---
+++ Tests/HTMLTests/HTMLTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HTML
+
+final class HTMLTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HTML().text, "Hello, World!")
+    }
+}
