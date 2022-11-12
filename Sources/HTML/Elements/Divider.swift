5b1c5f53afb33f45cdb7377ce4eec003ceefe9db
Vaida <91354917+Vaida12345@users.noreply.github.com>
2022-11-13 01:05:12 +1100

    update

---
+++ Sources/HTML/Elements/Divider.swift
@@ -0,0 +1,19 @@
+//
+//  Divider.swift
+//  
+//
+//  Created by Vaida on 11/12/22.
+//
+
+
+/// A thematic break (originally: horizontal rule).
+public struct Divider {
+    
+}
+
+
+extension Divider: Markup {
+    
+    public var body: Never { fatalError("Should never called body") }
+    
+}
